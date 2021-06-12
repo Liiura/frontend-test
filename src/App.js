250e6fc133943d151ec421f5887dcf5529f88674
Cognoceme <cognocemeapp@gmail.com>
2021-06-11 23:06:08 -0500

    delete mentors 2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from './components/Navbar'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Students from './pages/students/students'
-import Mentors from './pages/mentors/mentors'
 import LessonsCareer from './pages/lessonsCareer/lessosCareer'
 import Lessons from './pages/lessons/lessons'
 
@@ -16,7 +15,6 @@ function App() {
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/students' component={Students} />
-          <Route path='/mentors' component={Mentors} />
           <Route path='/lessonsCareer' component={LessonsCareer} />
           <Route path='/lessons' component={Lessons} />
         </Switch>
